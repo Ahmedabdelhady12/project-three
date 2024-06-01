39ff681f16cc0dbd31f9e6847063032050a84f9a
Ahmedabdelhady12 <164896991+Ahmedabdelhady12@users.noreply.github.com>
2024-06-01 23:00:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("btnt").onclick=function(){
+    document.getElementById("fder").innerHTML=alert("copy success")
+}
+document.getElementById("btnt12").onclick=function(){
+    document.getElementById("fder").innerHTML=alert("copy success")
+}
+document.getElementById("btnt13").onclick=function(){
+    document.getElementById("fder").innerHTML=alert("copy success")
+}
+var btn =document.querySelector("#div1hidden")
+document.getElementById("divhidden").onclick=function(){
+    document.innerHTML=btn.display="block"
+}
